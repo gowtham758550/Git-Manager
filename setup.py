80296ecb639e314e4bf2e2d9dae366540f07301b
Gowtham <gowtham758550@gmail.com>
2021-01-18 15:35:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import platform
 
 if platform.system().lower()=='windows':
-	print('Setup Doesnot Support Windows . \nPlease run " py main.py" at command prompt or powershell to run Git-manager...')
+	print('Setup Doesnot Support Windows . \nPlease run " py main.py" at command prompt or powershell to run Qgit...')
 	x=input('\nPress any key to continue...')
 	exit(0)
 
@@ -11,7 +11,7 @@ with open('shell.txt') as f:
  shell='/.'+f.read().strip().split('/')[-1]+'rc'
 
 cwd = os.getcwd() 
-alias="\nalias git-manager='python3 {}/main.py'".format(cwd)
+alias="\nalias qgit='python3 {}/main.py'".format(cwd)
 
 home = os.path.expanduser("~")
 if os.path.exists(home + shell)==False:
@@ -21,7 +21,7 @@ if os.path.exists(home + shell)==False:
 try:
 		with open(home + shell,'a') as f:
 			f.write(alias)
-			print("\nSetup file executed successfully\n\nNow you can use the tool by just typing 'git-manager' in any directory\n\nClose the current terminal and open new terminal to use our tool :)")
+			print("\nSetup file executed successfully\n\nNow you can use the tool by just typing 'qgit' in any directory\n\nClose the current terminal and open new terminal to use our tool :)")
 
 except:
 	print("Something went wrong , try running as Administrator.")
